ce19533115c1cda0c8ae89c9d45174228d2ed3de
cabralPorto <ronaldocabralporto@gmail.com>
2022-05-05 22:39:43 -0300

    Classe Modelo

---
+++ ProjetoAPICore/modelos/PassagemConteiner.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjetoAPICore.Modelos
+{
+    public class PassagemConteiner
+    {
+        public int IdPassagemConteiner { get; set; }
+        public int IdConteiner { get; set; }
+        public string Categoria { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public DateTime DataCancelamento { get; set; }
+    }
+}
---
+++ ProjetoAPICore/modelos/TipoMovimentacao.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoAPICore.Modelos
+{
+    public class TipoMovimentacao
+    {
+        public int IdTipoMovimentacao { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public DateTime DataCancelamento { get; set; }
+    }
+}
